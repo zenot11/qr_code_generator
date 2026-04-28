c51dfea0c8f605b0226da689ecc65f9efae237ef
zenot11 <akunimaks@gmail.com>
2026-04-28 22:53:55 +0300

    Convert QR generator to Flask app

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 import qrcode
+import base64
+from flask import Flask
+from flask import request
+from io import BytesIO
 
-img = qrcode.make('GitHub Webhook')
-img.save("gitHub_webhook.png")
+app = Flask(__name__)
+
+@app.route("/qr")
+def qr():
+    msg = request.args.get('msg')
+    img = qrcode.make(msg)
+
+    buffer = BytesIO()
+    img.save(buffer, format="png")
+
+    img64 = base64.b64encode(buffer.getvalue())
+    return f'<img src="data:image/png;base64, {img64.decode()}" alt="qrcode" />'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
